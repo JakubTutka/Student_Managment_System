26ea5108e670c0dab53f73c8619ae00f3755dc36
mikolajsztaba <mikolaj25sztaba@gmail.com>
2022-01-01 20:27:23 +0100

    Error handling 404 etc, improved layout and user experience

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from website import create_app
+from flask import render_template
+from flask_login import current_user
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# obsluga bledu 404, czyli np kiedy uzytkownik chce sobie wpisac /rokoko
+# to musi byc tu bo to jest po stronie przegladarki, inaczej nie dziala
+@app.errorhandler(404)
+def PageNotFound(error):
+    return render_template('404.html', user=current_user)
